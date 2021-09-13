6be059128b5f20aef3b2f8e3e08d955b0f45f9fa
Lê Văn Khải <khailv@meeyland.com>
2021-09-13 14:16:12 +0700

    migrade null safety

--- lib/map_pin_picker.dart
+++ lib/map_pin_picker.dart
@@ -3,21 +3,21 @@ library map_pin_picker;
 import 'package:flutter/material.dart';
 
 class MapPickerController {
-  Function mapMoving;
-  Function mapFinishedMoving;
+  Function()? mapMoving;
+  Function()? mapFinishedMoving;
 }
 
 class MapPicker extends StatefulWidget {
   final Widget child;
   final Widget iconWidget;
-  final bool showDot;
+  final bool? showDot;
   final MapPickerController mapPickerController;
 
   MapPicker(
-      {@required this.mapPickerController,
-      @required this.iconWidget,
+      {required this.mapPickerController,
+      required this.iconWidget,
       this.showDot = true,
-      @required this.child});
+      required this.child});
 
   @override
   _MapPickerState createState() => _MapPickerState();
@@ -25,16 +25,14 @@ class MapPicker extends StatefulWidget {
 
 class _MapPickerState extends State<MapPicker>
     with SingleTickerProviderStateMixin {
-  AnimationController animationController;
+  late AnimationController animationController;
 
   @override
   void initState() {
     animationController =
         AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    if (widget.mapPickerController != null) {
-      widget.mapPickerController.mapMoving = mapMoving;
-      widget.mapPickerController.mapFinishedMoving = mapFinishedMoving;
-    }
+    widget.mapPickerController.mapMoving = mapMoving;
+    widget.mapPickerController.mapFinishedMoving = mapFinishedMoving;
   }
 
   void mapMoving() {
@@ -52,7 +50,6 @@ class _MapPickerState extends State<MapPicker>
     return Stack(
       children: [
         widget.child,
-        Container(),
         AnimatedBuilder(
             animation: animationController,
             builder: (context, snapshot) {
@@ -65,14 +62,14 @@ class _MapPickerState extends State<MapPicker>
                       offset: Offset(0, -10 * animationController.value),
                       child: widget.iconWidget,
                     ),
-                    if(widget.showDot)
-                    Container(
-                      width: 5,
-                      height: 5,
-                      decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(5)),
-                    )
+                    if (widget.showDot ?? false)
+                      Container(
+                        width: 5,
+                        height: 5,
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(5)),
+                      )
                   ],
                 ),
               );
